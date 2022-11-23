ce9e08b7d4473c55884ef9cc86c1828ae9c042d3
yanisa.p <yanisa.p@gofive.co.th>
2022-11-23 16:24:54 +0700

    init form

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // public error = {
+	// 	email: 'common_validation_format_email',
+	// 	required: 'common_validation_required_info',
+	// 	duplicatedEmail: 'common_validation_duplicated_email',
+	// 	duplicatedCompany: 'common_validation_duplicated_company',
+	// 	duplicatedName: 'common_validation_duplicated_name',
+	// 	duplicatedPhone: 'common_validation_duplicated_phone',
+	// 	duplicatedDomain: 'common_validation_duplicated_domain',
+	// 	tel: 'common_validation_format_tel',
+	// }
+
+  public detailForm: FormGroup
+
+	get email() {
+		return this.detailForm.get('email')
+	}
+
+	get projectName() {
+		return this.detailForm.get('projectName')
+	}
+
+	get projectStatus() {
+		return this.detailForm.get('projectStatus')
+	}
+  
+  constructor(){
+    this.detailForm = new FormGroup({
+			projectName: new FormControl(null, [Validators.required]),
+      email: new FormControl('', [Validators.email]),
+			projectStatus: new FormControl(''),
+		})
+  }
+  onSubmit(){
+    console.log(this.detailForm)
+  }
+}
